6f6ca5678149cc033f30537fc970be7fe116e32a
Ivan Chistyakov <vancodx@gmail.com>
2025-08-05 23:22:35 +0300

    Fixed Validation.php

--- src/Validation.php
+++ src/Validation.php
@@ -4,6 +4,26 @@ namespace VanCodX\Data\Assertion;
 
 use VanCodX\Data\Validation\Validation as BaseValidation;
 
-class Validation extends BaseValidation
-{
+if (defined('IS_PHPUNIT_RUNNING') && IS_PHPUNIT_RUNNING) {
+    class Validation
+    {
+        /**
+         * @var BaseValidation|class-string<BaseValidation>
+         */
+        protected static BaseValidation|string $mock = BaseValidation::class;
+
+        /**
+         * @param string $name
+         * @param array $arguments
+         * @return mixed
+         */
+        public static function __callStatic(string $name, array $arguments): mixed
+        {
+            return static::$mock::$name(...$arguments);
+        }
+    }
+} else {
+    class Validation extends BaseValidation
+    {
+    }
 }
