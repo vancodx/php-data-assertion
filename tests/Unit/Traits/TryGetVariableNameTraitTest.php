3042b6786c6e0bfbc894441ef9aa3881903a723a
Ivan Chistyakov <vancodx@gmail.com>
2025-08-12 23:03:42 +0300

    Fixed TryGetVariableNameTraitTest.php

--- tests/Unit/Traits/TryGetVariableNameTraitTest.php
+++ tests/Unit/Traits/TryGetVariableNameTraitTest.php
@@ -16,34 +16,36 @@ class TryGetVariableNameTraitTest extends MockeryTestCase
 
         // A::argIsBool($code42);
 
-        $mock->expects('debugBacktrace')->once()->with(DEBUG_BACKTRACE_IGNORE_ARGS, 2)->andReturn([
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
-            ['file' => __FILE__, 'line' => 17, 'function' => 'argIsBool'],
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
-        ]);
-        $mock->expects('debugBacktrace')->once()->with(DEBUG_BACKTRACE_IGNORE_ARGS, 2)->andReturn([
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
-            ['file' => __FILE__, 'line' => 17, 'function' => 'argIsNull'], // incorrect function
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
-        ]);
-        $mock->expects('debugBacktrace')->once()->with(DEBUG_BACKTRACE_IGNORE_ARGS, 2)->andReturn([
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
-            ['file' => 'non-existent-file', 'line' => 17, 'function' => 'argIsBool'], // non-existent file
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
-        ]);
-        $mock->expects('debugBacktrace')->once()->with(DEBUG_BACKTRACE_IGNORE_ARGS, 2)->andReturn([
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
-            ['line' => 17, 'function' => 'argIsBool'], // no file
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
-        ]);
-        $mock->expects('debugBacktrace')->once()->with(DEBUG_BACKTRACE_IGNORE_ARGS, 2)->andReturn([
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
-            ['file' => __FILE__, 'function' => 'argIsBool'], // no line
-            ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
-        ]);
-        $mock->expects('debugBacktrace')->once()->with(DEBUG_BACKTRACE_IGNORE_ARGS, 2)->andReturn([
-            ['file' => __FILE__, 'line' => 17, 'function' => 'argIsBool'] // not enough records
-        ]);
+        $mock->expects('debugBacktrace')->times(6)->with(DEBUG_BACKTRACE_IGNORE_ARGS, 2)->andReturn(
+            [
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
+                ['file' => __FILE__, 'line' => 17, 'function' => 'argIsBool'],
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
+            ],
+            [
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
+                ['file' => __FILE__, 'line' => 17, 'function' => 'argIsNull'], // incorrect function
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
+            ],
+            [
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
+                ['file' => 'non-existent-file', 'line' => 17, 'function' => 'argIsBool'], // non-existent file
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
+            ],
+            [
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
+                ['line' => 17, 'function' => 'argIsBool'], // no file
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
+            ],
+            [
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function'],
+                ['file' => __FILE__, 'function' => 'argIsBool'], // no line
+                ['file' => __FILE__, 'line' => 1, 'function' => 'non-existent-function']
+            ],
+            [
+                ['file' => __FILE__, 'line' => 17, 'function' => 'argIsBool'] // not enough records
+            ]
+        );
         $mock->expects('debugBacktrace')->never();
 
         $this->assertSame('code42', $mock::tryGetVariableName());
